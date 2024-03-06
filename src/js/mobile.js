ec57f2171fe23971b651cb0316aca5ed39255f84
Yurii Kyrychenko <paravoznik88@gmail.com>
2024-03-06 23:30:21 +0200

    upd-js

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -23,20 +23,20 @@ document.addEventListener('DOMContentLoaded', function () {
     menuMobile.classList.remove('is-open');
     backdrop.classList.remove('is-open');
   });
-});
 
-const header = document.querySelector('.top');
-const sectionWork = document.querySelector('.section-work');
+  const header = document.querySelector('.top');
+  const sectionWork = document.querySelector('.section-work');
 
-function handleHeaderStyle() {
-  if (window.innerWidth <= 1279) {
-    header.style.backgroundColor = 'rgba(87, 111, 53, 0.8)';
-  } else if (window.scrollY >= sectionWork.offsetTop) {
-    header.style.backgroundColor = 'rgba(87, 111, 53, 0.8)';
-  } else {
-    header.style.backgroundColor = 'transparent';
+  function handleHeaderStyle() {
+    if (window.innerWidth <= 1279) {
+      header.style.backgroundColor = 'rgba(87, 111, 53, 0.8)';
+    } else if (window.scrollY >= sectionWork.offsetTop) {
+      header.style.backgroundColor = 'rgba(87, 111, 53, 0.8)';
+    } else {
+      header.style.backgroundColor = 'transparent';
+    }
   }
-}
 
-window.addEventListener('scroll', handleHeaderStyle);
-window.addEventListener('resize', handleHeaderStyle);
+  window.addEventListener('scroll', handleHeaderStyle);
+  window.addEventListener('resize', handleHeaderStyle);
+});
