acaf15b320bdd2d1f65109d771984712e5e67899
Yurii Kyrychenko <paravoznik88@gmail.com>
2024-03-06 23:03:09 +0200

    upd-header-footer-vegetables

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,31 +1,3 @@
-// (() => {
-//   const mobileMenu = document.querySelector('.js-menu-container');
-//   const openMenuBtn = document.querySelector('.js-open-menu');
-//   const closeMenuBtn = document.querySelector('.js-close-menu');
-
-//   const toggleMenu = () => {
-//     const isMenuOpen =
-//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//     mobileMenu.classList.toggle('is-open');
-
-//     const scrollLockMethod = !isMenuOpen
-//       ? 'disableBodyScroll'
-//       : 'enableBodyScroll';
-//     bodyScrollLock[scrollLockMethod](document.body);
-//   };
-
-//   openMenuBtn.addEventListener('click', toggleMenu);
-//   closeMenuBtn.addEventListener('click', toggleMenu);
-
-//   // Close the mobile menu on wider screens if the device orientation changes
-//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-//     if (!e.matches) return;
-//     mobileMenu.classList.remove('is-open');
-//     openMenuBtn.setAttribute('aria-expanded', false);
-//     bodyScrollLock.enableBodyScroll(document.body);
-//   });
-// })();
 document.addEventListener('DOMContentLoaded', function () {
   const links = document.querySelectorAll('.menu-link, .button-menu-mobile');
   const backdrop = document.querySelector('.backdrop');
@@ -52,3 +24,19 @@ document.addEventListener('DOMContentLoaded', function () {
     backdrop.classList.remove('is-open');
   });
 });
+
+const header = document.querySelector('.top');
+const sectionWork = document.querySelector('.section-work');
+
+function handleHeaderStyle() {
+  if (window.innerWidth <= 1279) {
+    header.style.backgroundColor = 'rgba(87, 111, 53, 0.8)';
+  } else if (window.scrollY >= sectionWork.offsetTop) {
+    header.style.backgroundColor = 'rgba(87, 111, 53, 0.8)';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+}
+
+window.addEventListener('scroll', handleHeaderStyle);
+window.addEventListener('resize', handleHeaderStyle);
