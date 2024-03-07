53ed5ef46a67658edd1085b22470d84743e2da14
OlesyaAlexx <152548843+OlesyaAlexx@users.noreply.github.com>
2024-03-07 18:51:09 +0200

    js how-it-work

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -52,3 +52,19 @@ document.addEventListener('DOMContentLoaded', function () {
     backdrop.classList.remove('is-open');
   });
 });
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    } else {
+      change.target.classList.remove('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.work-item');
+for (let elm of elements) {
+  observer.observe(elm);
+}
